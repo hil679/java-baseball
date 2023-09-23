e87f46a15239533fbe24933a947744eceb06d4d0
hil679 <hil679922@gmail.com>
2023-09-23 11:40:26 +0900

    get random number by computer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
+    private static Map<String, Integer> returnList = new HashMap<>();
+//    List<String> answer = new ArrayList<>();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<String> answer = getRandomAnswer();
+
+
     }
+
+    public static List<String> getRandomAnswer(){
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            answer.add(String.valueOf(Randoms.pickNumberInRange(0,9)));
+        }
+        return answer;
+    }
+
 }
